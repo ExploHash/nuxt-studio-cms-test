21663ba275c7b918351364c78709363120507e12
JBeaart <github@jbeaart.com>
2024-07-25 00:44:43 +0200

    1 change by Nuxt Studio

--- nuxt-studio/app.config.ts
+++ nuxt-studio/app.config.ts
@@ -1,1 +1,10 @@
-export default defineAppConfig({})
+export default defineAppConfig({
+  ui: {
+    primaryColor: "blue"
+  },
+
+  seo: {
+    title: "Dit is een titel",
+    description: "Dit is een description"
+  }
+})
