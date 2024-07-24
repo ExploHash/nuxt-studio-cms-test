275b796bc362679935e91c1a8e2c59425b7cdde8
ExploHash <jbtwaalf@gmail.com>
2024-07-25 00:44:56 +0200

    feat: updated schema

--- nuxt-studio/nuxt.schema.ts
+++ nuxt-studio/nuxt.schema.ts
@@ -12,7 +12,6 @@ export default defineNuxtSchema({
           title: 'Primary',
           description: 'Primary color of your UI.',
           icon: 'i-ph-palette',
-          default: 'sky',
           required: ['blue', 'green', 'red'],
         })
       }
@@ -20,21 +19,19 @@ export default defineNuxtSchema({
     seo: group({
       title: 'SEO',
       description: 'SEO configuration',
-      icon: 'i-ph-search-fill',
+      icon: 'i-ph-palette-fill',
       fields: {
         title: field({
           type: 'string',
           title: 'Title',
           description: 'Default title for your website.',
           icon: 'i-ph-search',
-          default: 'My Nuxt Studio App',
         }),
         description: field({
           type: 'string',
           title: 'Description',
           description: 'Default description for your website.',
           icon: 'i-ph-search',
-          default: 'My Nuxt Studio App',
         }),
       }
     }),
