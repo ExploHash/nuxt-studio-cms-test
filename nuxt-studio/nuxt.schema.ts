245306fe284c8eb2b3d9ef850024b505e301f7b7
ExploHash <jbtwaalf@gmail.com>
2024-07-25 00:42:23 +0200

    feat;: added layout and better schema

--- nuxt-studio/nuxt.schema.ts
+++ nuxt-studio/nuxt.schema.ts
@@ -2,20 +2,41 @@ import { field, group } from '@nuxthq/studio/theme'
 
 export default defineNuxtSchema({
   appConfig: {
-    parent: group({
+    ui: group({
       title: 'UI',
       description: 'UI configuration',
       icon: 'i-ph-palette-fill',
       fields: {
-        primary: field({
+        primaryColor: field({
           type: 'string',
           title: 'Primary',
           description: 'Primary color of your UI.',
           icon: 'i-ph-palette',
           default: 'sky',
-          required: ['sky', 'mint', 'rose', 'amber']
+          required: ['blue', 'green', 'red'],
         })
       }
-    })
+    }),
+    seo: group({
+      title: 'SEO',
+      description: 'SEO configuration',
+      icon: 'i-ph-search-fill',
+      fields: {
+        title: field({
+          type: 'string',
+          title: 'Title',
+          description: 'Default title for your website.',
+          icon: 'i-ph-search',
+          default: 'My Nuxt Studio App',
+        }),
+        description: field({
+          type: 'string',
+          title: 'Description',
+          description: 'Default description for your website.',
+          icon: 'i-ph-search',
+          default: 'My Nuxt Studio App',
+        }),
+      }
+    }),
   }
 })
